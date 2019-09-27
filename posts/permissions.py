817e0425e07a7993d01eabcc28861a3c15d46818
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-09-27 04:35:46 +0000

    HOTFIX

--- posts/permissions.py
+++ posts/permissions.py
@@ -7,7 +7,7 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view,obj):
         # Read-only permissions are allowed for any request
-        if request.method in permissions.SAFE_METHOD:
+        if request.method in permissions.SAFE_METHODS:
             return True
 
         # Write permissions are only allowed to the author of a post
