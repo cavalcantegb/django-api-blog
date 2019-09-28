bf33a9ae4799916d3333052810753e588958fa74
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-09-28 11:12:20 +0000

    Hotfix on urls.py

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('posts.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/v1/rest-atuh/', include('rest_auth.urls')),
+    path('api/v1/rest-auth/', include('rest_auth.urls')),
 ]
