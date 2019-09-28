f2c9e103bfa8ead0e87224e55919e274357d6df0
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-09-28 10:56:24 +0000

    Add authentication to the api

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -48,6 +48,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ]
 }
 
