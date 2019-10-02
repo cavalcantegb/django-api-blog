c141620bc9e45aca186126e235d876c7b20c4331
Guilherme Cavalcante <cavalcante.guilherme91@gmail.com>
2019-10-02 13:28:41 +0000

    Creating Users Endpoints

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -7,3 +8,8 @@ class PostSerializer(serializers.ModelSerializer):
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
